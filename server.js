75ef848a42a7693f4b9421cea804eeab57b24228
yakintech <yildiz.cagatay@hotmail.com>
2019-09-23 10:18:18 +0300

    hello socketIO!

---
+++ server.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+  app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+  });
+  app.get('/chat', function (req, res) {
+    res.sendfile(__dirname + '/chat.html');
+  });
+
+  server.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+
+
+  io.on('connection', function(socket){
+    var mesajlar = [];
+
+    var sid = socket.id;
+      socket.on('servermsg', function(data){
+        var mesaj = new Object();
+        mesaj.id = sid;
+        mesajlar.push(mesaj);
+
+        var count = mesajlar.length - 1;
+        var mesajdurum = false;
+        if(count > 3)
+        {
+          mesajdurum = mesajlar[count].id == sid && mesajlar[count - 1].id == sid;
+        }
+       
+        if(data.content.length < 50 && !mesajdurum){
+          io.emit('clientmsg',data);
+        }
+        else{
+          io.to(sid).emit("banfunc")
+          console.log("BAN! " + sid);
+        }
+      
+      });
+   });
